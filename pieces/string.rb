d45eecfed4d41d6220a453dbe7917880de00e08b
Dmitry <an1ic@mail.ru>
2015-10-20 16:15:46 +0600

    Update string.rb

--- pieces/string.rb
+++ pieces/string.rb
@@ -2,6 +2,6 @@ class String
 	def to_real_item
 		fields = self.chomp # "car:100:50"
 		fields = fields.split(":") # ["car", "100", "50"]
-		RealItem.new(name: i[0], price: i[1].to_i, weight: i[2].to_i)
+		RealItem.new(name: i[0].to_i, price: i[1].to_i, weight: i[2].to_i)
 	end
 end
